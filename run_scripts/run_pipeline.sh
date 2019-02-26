3a4aa5425f453910a3eaf6c33dcc45646fbae5ff
Alexander Simpson <as2388@cam.ac.uk>
2019-02-26 17:02:00 +0000

    Rename all.sh -> run_pipeline.sh and add help string

--- run_scripts/run_pipeline.sh
+++ run_scripts/run_pipeline.sh
@@ -3,8 +3,11 @@
 set -e
 
 if [[ $# -ne 10 ]]; then
-    echo "Usage: "
-    echo "Runs the pipeline end-to-end (data fetch, coda fetch, output generation, Drive upload, coda upload, data backup)"
+    echo "Usage: ./run_pipeline.sh"
+    echo "  <user> <coda-pull-credentials-path> <coda-push-credentials-path> <avf-bucket-credentials-path>"
+    echo "  <drive-service-account-credentials-url> <rapid-pro-tools-root> <coda-tools-root> <data-root>"
+    echo "  <drive-upload-dir> <data-backup-dir>"
+    echo "Runs the pipeline end-to-end (data fetch, coda fetch, output generation, Drive upload, Coda upload, data backup)"
     exit
 fi
 
@@ -17,7 +20,7 @@ RAPID_PRO_TOOLS_ROOT=$6
 CODA_TOOLS_ROOT=$7
 DATA_ROOT=$8
 DRIVE_UPLOAD_DIR=$9
-DATA_PREV=${10}
+DATA_BACKUPS_DIR=${10}
 
 ./1_coda_get.sh "$CODA_PULL_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
 
@@ -31,7 +34,7 @@ pipenv run python 2_fetch_raw_data.py "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" ../
 # Backup the project data directory
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 HASH=$(git rev-parse HEAD)
-mkdir -p "$DATA_PREV"
+mkdir -p "$DATA_BACKUPS_DIR"
 find "$DATA_ROOT" -type f -name '.DS_Store' -delete
 cd "$DATA_ROOT"
-tar -czvf "$DATA_PREV/data-$DATE-$HASH.tar.gzip" .
+tar -czvf "$DATA_BACKUPS_DIR/data-$DATE-$HASH.tar.gzip" .
