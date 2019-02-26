758c8a1e43c70b64868007928d2c889b798cbfa9
Alexander Simpson <as2388@cam.ac.uk>
2019-02-26 16:13:28 +0000

    Fix gcloud login not working

--- run_scripts/setup_ubuntu_vm.sh
+++ run_scripts/setup_ubuntu_vm.sh
@@ -34,6 +34,7 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
     echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
     sudo apt update && sudo apt install -y google-cloud-sdk
+sudo chown -R "$USER" "/home/$USER/.config/gcloud"
 
 # Install project dependencies
 pipenv --three
