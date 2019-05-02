0ce722604a31e88c03e7c3b310600cb3048cd33a
Alexander Simpson <as2388@cam.ac.uk>
2019-05-02 12:45:25 +0300

    Move show key re-mapper from TranslateRapidProKeys to configuration json

--- src/lib/pipeline_configuration.py
+++ src/lib/pipeline_configuration.py
@@ -365,13 +365,17 @@ class PipelineConfiguration(object):
 
 
 class RapidProKeyRemapping(object):
-    def __init__(self, rapid_pro_key, pipeline_key):
+    def __init__(self, activation_message, rapid_pro_key, pipeline_key):
         """
+        :param activation_message: Whether this re-mapping contains an activation message (activation messages need
+                                   to be handled differently because they are not always in the correct flow)
+        :type activation_message: bool
         :param rapid_pro_key: Name of key in the dataset exported via RapidProTools.
         :type rapid_pro_key: str
         :param pipeline_key: Name to use for that key in the rest of the pipeline.
         :type pipeline_key: str
         """
+        self.activation_message = activation_message
         self.rapid_pro_key = rapid_pro_key
         self.pipeline_key = pipeline_key
         
@@ -379,12 +383,14 @@ class RapidProKeyRemapping(object):
 
     @classmethod
     def from_configuration_dict(cls, configuration_dict):
+        activation_message = configuration_dict.get("ActivationMessage", False)
         rapid_pro_key = configuration_dict["RapidProKey"]
         pipeline_key = configuration_dict["PipelineKey"]
         
-        return cls(rapid_pro_key, pipeline_key)
+        return cls(activation_message, rapid_pro_key, pipeline_key)
     
     def validate(self):
+        validators.validate_bool(self.activation_message, "activation_message")
         validators.validate_string(self.rapid_pro_key, "rapid_pro_key")
         validators.validate_string(self.pipeline_key, "pipeline_key")
 
