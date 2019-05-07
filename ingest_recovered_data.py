55e64f5a4b5ef02050f0a8811d37dc4a0e4737c4
Alexander Simpson <as2388@cam.ac.uk>
2019-05-07 09:57:02 +0300

    Ingest recovered data from a bucket using pipeline configuration
    (changed from reading from a local file)

--- ingest_recovered_data.py
+++ ingest_recovered_data.py
@@ -1,12 +1,16 @@
 import argparse
 import csv
 from datetime import datetime
+from io import StringIO
 
 import pytz
 from core_data_modules.logging import Logger
 from core_data_modules.traced_data import Metadata, TracedData
 from core_data_modules.traced_data.io import TracedDataJsonIO
 from core_data_modules.util import PhoneNumberUuidTable, TimeUtils, SHAUtils
+from storage.google_cloud import google_cloud_utils
+
+from src.lib import PipelineConfiguration
 
 log = Logger(__name__)
 
@@ -16,55 +20,66 @@ if __name__ == "__main__":
                                                  "which can be processed by the pipeline")
 
     parser.add_argument("user", help="Identifier of the user launching this program")
-    parser.add_argument("recovered_csv_input_path", metavar="recovered-csv-input-path",
-                        help="Path to a CSV file containing the recovered data")
+    parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
+                        help="Path to a Google Cloud service account credentials file to use to access the "
+                             "credentials bucket")
+    parser.add_argument("pipeline_configuration_file_path", metavar="pipeline-configuration-file",
+                        help="Path to the pipeline configuration json file")
     parser.add_argument("phone_number_uuid_table_path", metavar="phone-number-uuid-table-path",
                         help="Path to the phone number <-> uuid table")
-    parser.add_argument("traced_data_output_path", metavar="traced-data-output-path",
-                        help="Path to a JSON file to write the serialized data output to")
+    parser.add_argument("raw_data_dir", metavar="raw-data-dir",
+                        help="Path to a directory to save the raw data to")
 
     args = parser.parse_args()
 
     user = args.user
-    recovered_csv_input_path = args.recovered_csv_input_path
+    google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
+    pipeline_configuration_file_path = args.pipeline_configuration_file_path
     phone_number_uuid_table_path = args.phone_number_uuid_table_path
-    dataset_id = args.dataset_id
-    traced_data_output_path = args.traced_data_output_path
+    raw_data_dir = args.raw_data_dir
 
-    log.info(f"Reading recovered data from '{recovered_csv_input_path}'...")
-    with open(recovered_csv_input_path, "r") as f:
-        raw_data = list(csv.DictReader(f))
-    log.info(f"Read {len(raw_data)} recovered messages")
+    # Load the pipeline configuration file
+    log.info("Loading Pipeline Configuration File...")
+    with open(pipeline_configuration_file_path) as f:
+        pipeline_configuration = PipelineConfiguration.from_configuration_file(f)
 
     log.info(f"Loading the phone number <-> uuid table from '{phone_number_uuid_table_path}'...")
     with open(phone_number_uuid_table_path) as f:
         phone_number_uuid_table = PhoneNumberUuidTable.load(f)
     log.info(f"Loaded {len(phone_number_uuid_table.numbers())} phone number <-> uuid mappings")
 
-    log.info("Converting the recovered messages to TracedData...")
-    data = []
-    for i, row in enumerate(raw_data):
-        raw_date = row["Received On"]
-        parsed_raw_date = datetime.strptime(raw_date, "%m/%d/%y %H:%M")
-        localized_date = pytz.timezone("Africa/Mogadishu").localize(parsed_raw_date)
+    for recovery_csv_url in pipeline_configuration.recovery_csv_urls:
+        log.info(f"Downloading recovered data from '{recovery_csv_url}'...")
+        raw_csv_string = StringIO(google_cloud_utils.download_blob_to_string(
+            google_cloud_credentials_file_path, recovery_csv_url))
+        raw_data = list(csv.DictReader(raw_csv_string))
+        log.info(f"Downloaded {len(raw_data)} recovered messages")
 
-        d = {
-            "avf_phone_id": phone_number_uuid_table.add_phone(row["Mobile No"]),
-            "message": row["Message Content"],
-            "received_on": localized_date.isoformat(),
-            "run_id": SHAUtils.sha_dict(row)
-        }
+        log.info("Converting the recovered messages to TracedData...")
+        data = []
+        for i, row in enumerate(raw_data):
+            raw_date = row["Received On"]
+            parsed_raw_date = datetime.strptime(raw_date, "%m/%d/%y %H:%M")
+            localized_date = pytz.timezone("Africa/Mogadishu").localize(parsed_raw_date)
 
-        data.append(TracedData(d, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string())))
-    log.info("Converted the recovered messages to TracedData...")
+            d = {
+                "avf_phone_id": phone_number_uuid_table.add_phone(row["Mobile No"]),
+                "message": row["Message Content"],
+                "received_on": localized_date.isoformat(),
+                "run_id": SHAUtils.sha_dict(row)
+            }
 
-    log.info(f"Updating the phone number <-> uuid table at '{phone_number_uuid_table_path}' "
-             f"with {len(phone_number_uuid_table.numbers())} phone number <-> uuid mappings...")
-    with open(phone_number_uuid_table_path, "w") as f:
-        phone_number_uuid_table.dump(f)
-    log.info(f"Updated the phone number <-> uuid table")
+            data.append(TracedData(d, Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string())))
+        log.info("Converted the recovered messages to TracedData...")
 
-    log.info(f"Exporting {len(data)} TracedData items to {traced_data_output_path}...")
-    with open(traced_data_output_path, "w") as f:
-        TracedDataJsonIO.export_traced_data_iterable_to_json(data, f, pretty_print=True)
-    log.info(f"Exported TracedData")
+        log.info(f"Updating the phone number <-> uuid table at '{phone_number_uuid_table_path}' "
+                 f"with {len(phone_number_uuid_table.numbers())} phone number <-> uuid mappings...")
+        with open(phone_number_uuid_table_path, "w") as f:
+            phone_number_uuid_table.dump(f)
+        log.info(f"Updated the phone number <-> uuid table")
+
+        traced_data_output_path = f"{raw_data_dir}/{recovery_csv_url.split('/')[-1].split('.')[0]}.json"
+        log.info(f"Exporting {len(data)} TracedData items to {traced_data_output_path}...")
+        with open(traced_data_output_path, "w") as f:
+            TracedDataJsonIO.export_traced_data_iterable_to_json(data, f, pretty_print=True)
+        log.info(f"Exported TracedData")
